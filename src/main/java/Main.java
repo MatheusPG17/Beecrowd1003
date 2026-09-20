707fd9b1aa364f88d45ba0f819b548d5e57b9a60
Matheus Pereira Gonçalves Coelho <mpg9159@gmail.com>
2026-09-20 04:03:55 -0300

    uptade adicionando comentários como commit teste

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,13 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
+        //variaveis
         int A, B, SOMA;
-        
+        //entrada de dados
         A = leia.nextInt();
         B = leia.nextInt();
-        
+        //Processamento
         SOMA = (A + B);
-        
+        //saida de dados
         System.out.println("SOMA = " + SOMA);
     }
 }
